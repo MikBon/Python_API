ce3b0ad8fe4df071073a2ad73b4aab035221b37b
MikBon <157021505+MikBon@users.noreply.github.com>
2025-05-12 22:18:02 +0300

    PR6

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from app import create_app
 from db import db
-import models.store, models.tag
+import models.store, models.tag, models.item, models.user
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Не викликаємо app.run() – Gunicorn зробить це сам
