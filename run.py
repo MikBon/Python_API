85e322849c1f7e63a1b7f75b18576b95e29a4fe5
MikBon <157021505+MikBon@users.noreply.github.com>
2025-05-13 09:05:24 +0300

    run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from app import app
+from app import create_app
 from db import db
+from models import user, item, store, tag  
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+app = create_app()
+
+with app.app_context():
+    db.create_all()
